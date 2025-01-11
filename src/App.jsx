f97c113a97cfd38541b8d8e0a5e4688ffc310675
imadJHR <imadjohar4@gmail.com>
2025-01-11 15:27:39 +0100

    remove pricing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Pricing from "./components/Pricing";
 import {  ToastContainer } from "react-toastify";
 import Footer from "./components/Footer";
 const App = () => {
@@ -14,7 +13,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
-      <Pricing/>
+      
       <Contact />
       <Footer/>
     </div>
