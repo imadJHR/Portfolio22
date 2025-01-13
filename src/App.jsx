450ca2570cd47fbae2e50769fa7b2427ede5743f
imadJHR <imadjohar4@gmail.com>
2025-01-13 15:40:44 +0100

    fefe

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,19 @@ import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import {  ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import Footer from "./components/Footer";
 import Pricing from "./components/Pricing";
 const App = () => {
   return (
     <div>
-      <ToastContainer/>
+      <ToastContainer />
       <Navbar />
       <Hero />
       <About />
       <Projects />
-      <Pricing/>
-      
       <Contact />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
